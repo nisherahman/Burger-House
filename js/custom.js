8f76066bc29c629ab8986e3d1a88c656e1589606
Nishe Rahman <91620950+nisherahman@users.noreply.github.com>
2022-05-01 21:11:38 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,37 @@
 $(document).ready(function(){
 
-    //slick slider
-    $('.event-slider').slick({
-        arrows:false,
-        dots:true,
-        // autoplay:true,
-      });
+   // scroll position
+   $(window).on('scroll',function(){
+    var scrolling = $(this).scrollTop();
+
+    // back to top appearance 
+    if(scrolling > 100){
+      $('.back-to-top').fadeIn();
+    }else{
+      $('.back-to-top').fadeOut();  
+    }
+    // navbar class 
+    if(scrolling > 150){
+      $('nav').addClass('nav-fix');
+    }else{
+      $('nav').removeClass('nav-fix');
+    }
+
+  });
+
+  // back to top action 
+  $('.back-to-top').on('click',function(){
+    $('html,body').animate({
+    scrollTop: '0px',
+  },2000);
+
+  });    
+
+  //slick slider
+  $('.event-slider').slick({
+    arrows:false,
+    dots:true,
+    // autoplay:true,
+  });
   
 });   
